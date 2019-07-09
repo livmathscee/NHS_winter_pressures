ff3f05695635a1fec43149b1938419f44de45802
Liverpool Maths CEE <mathscee@liverpool.ac.uk>
2019-07-09 10:22:44 +0100

    Create simplified teaching version of code, with associated worksheet and files

--- WPA_simplified.R
+++ WPA_simplified.R
@@ -1,3 +1,4 @@
+# Modified version of:
 #===============================================================================
 # Analysis of NHS winter sitrep data 2018/2019
 # Purpose: import, clean and generate STP map of bed occupancy by month
@@ -15,40 +16,13 @@ library(broom)
 library(maptools)
 
 
-# 2. Downloading winter data from NHS Digital -----------------------------
+# 2. Locate timeseries data -----------------------------------------------
 
 filename <- "Winter-data-timeseries-20190307.xlsx"
-url <- "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2019/03/Winter-data-timeseries-20190307.xlsx"
-
-
-download.file(url, destfile = filename, mode = "wb")
+#url <- "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2019/03/Winter-data-timeseries-20190307.xlsx"
 
 
 # 3. Importing data from R-unfriendly spreadsheets ------------------------
-
-### Exploring the double header problem
-
-example_indicator <- "G&A beds"
-
-# First line containes dates in merged cells (5 cells merged each in this case)
-header_1 <- read_xlsx(path = filename, sheet = example_indicator, 
-                      skip = 13, col_names = FALSE, 
-                      n_max = 1)
-
-# Second line contains variable names
-header_2 <- read_xlsx(path = filename, sheet = example_indicator, 
-                      skip = 14, col_names = FALSE, 
-                      n_max = 1)
-
-dim(header_1)
-dim(header_2)
-
-# Start of header 1
-header_1[1:10]
-
-# End of header 1
-header_1[(ncol(header_1)-10):ncol(header_1)]
-
 
 ### A function to import sitrep data
 import_sitrep <- function(file, indicator){
@@ -100,35 +74,26 @@ import_sitrep <- function(file, indicator){
   data_tidy
 }
 
+### Import data and add columns
 
-### Import and combine sheets
-sheets_to_import <- c("G&A beds", "Beds Occ by long stay patients")
-
-Sitrep_daily <- sheets_to_import %>% 
+Sitrep_daily <- "G&A beds" %>% 
   map(import_sitrep, 
       file = filename) %>% 
   reduce(left_join, 
          by = c("nhs.england.region", "code", "name", "date"))
-
-# What does our data look like now?
-dim(Sitrep_daily)
-
-head(Sitrep_daily)
 
 
 # 4. Data cleaning --------------------------------------------------------
 
 ### Trust Exclusions
 
-# children's hospitals to be exlcuded for aggregation by STP
+# children's hospitals to be excluded for aggregation by STP
 trusts_to_exclude_for_aggregation <- c("RQ3", "RBS", "RCU")
-
 
 ### Finding  periods of missing data
 
-# Only check variables that are not derived from other variables
-cols_to_check <- c("core.beds.open", "total.beds.occd", 
-                   "more.than.7.days", "more.than.14.days", "more.than.21.days")
+# Only check variables of interest that are not derived from other variables
+cols_to_check <- c("core.beds.open", "total.beds.occd")
 
 # Find values that are 0 or NA
 # within any trust/variable combination
@@ -152,8 +117,6 @@ Days_missing <- Sitrep_missing_or_zero %>%
 
 ### Longer gaps (4 or more days)
 
-print(Days_missing[Days_missing$days >= 4,])
-
 trusts_to_exclude <- Days_missing %>% 
   filter(days >= 4) %>% 
   ungroup() %>% 
@@ -161,28 +124,10 @@ trusts_to_exclude <- Days_missing %>%
   pull(code) %>% 
   unique()
 
-print(trusts_to_exclude)
-
-
 Sitrep_daily <- Sitrep_daily %>% 
   filter(!is.element(code, trusts_to_exclude))
 
-dim(Sitrep_daily)
-
 ### Dealing with shorter gaps
-
-# How many 1,2 and 3-day gaps are there?
-Days_missing %>% 
-  filter(!is.element(code, trusts_to_exclude)) %>% 
-  group_by(days) %>% 
-  count()
-
-# How are they distributed between trusts and variables?
-Days_missing %>% 
-  filter(!is.element(code, trusts_to_exclude)) %>% 
-  group_by(code, variable) %>% 
-  count() %>% 
-  spread(key = "variable", value = "n")
 
 # Extract and plot trusts with zeros in their data. 
 Sitrep_daily_small_gaps <- Sitrep_daily %>% 
@@ -199,7 +144,6 @@ ggplot(Sitrep_daily_small_gaps, aes(x = date, y = value, group = code, color = c
 # Create a 'clean' version where 0s were replaced with NA
 Sitrep_daily[cols_to_check] <- na_if(Sitrep_daily[cols_to_check], 0)
 
-
 ### Validating derived variables
 
 Sitrep_daily <- Sitrep_daily %>% 
@@ -211,11 +155,10 @@ all(round(Sitrep_daily$occupancy.rate, 6) == round(Sitrep_daily$occupancy.rate.c
 
 all(Sitrep_daily$total.beds.open == Sitrep_daily$total.beds.open.check)
 
-
 # Where are the mismatches?
 Sitrep_daily[Sitrep_daily$total.beds.open != Sitrep_daily$total.beds.open.check, 
              c("code", "date", "core.beds.open", "escalation.beds.open", "total.beds.open", 
-               "total.beds.open.check")]
+              "total.beds.open.check")]
 
 # Looks like we will have to re-derive both variables after all
 Sitrep_daily <- Sitrep_daily %>% 
@@ -223,113 +166,39 @@ Sitrep_daily <- Sitrep_daily %>%
          occupancy.rate = total.beds.occd / (core.beds.open + escalation.beds.open))
 
 
-# 5. Feature engineering --------------------------------------------------
+# 5. Adding organisational information: Sustainability and Transformation Partnerships (STPs)
 
-### Adding organisational information: Sustainability and Transformation Partnerships (STPs)
-
-# ideally save this table as a csv for future use
 STP_lookup <- read_csv("Trust-STP-lookup.csv")
 Sitrep_daily <- Sitrep_daily %>%
   left_join(STP_lookup[c('code', 'STP', 'STP_code')], by = c("code"))
 
 
-### Making read-outs comparable between trusts: from raw counts to rates
-
-# rate of occupied beds that are occupied by long-stay patients
-Sitrep_daily <- Sitrep_daily %>% 
-  mutate(more.than.7.rate = more.than.7.days / total.beds.occd,
-         more.than.14.rate = more.than.14.days / total.beds.occd,
-         more.than.21.rate = more.than.21.days / total.beds.occd)
-
-
-# 6. Aggregation: monthly averages by STP ---------------------------------
+# 6. Aggregation: monthly averages by STP ---------------------------------
 
 Sitrep_daily <- Sitrep_daily %>% 
-  mutate(week_start = as.Date(cut(date, breaks = "week", start.on.monday = TRUE)), #can be used to aggregate by week
-         month = format(date, format = "%B"))
-
-Sitrep_daily_STP <- Sitrep_daily %>% 
-  filter(!is.element(code, trusts_to_exclude_for_aggregation)) %>%
-  group_by(STP, STP_code, date) %>%
-  summarize(occupancy.rate.valid = sum(!is.na(occupancy.rate)),
-            more.than.7.rate.valid = sum(!is.na(more.than.7.rate)),
-            more.than.14.rate.valid = sum(!is.na(more.than.14.rate)),
-            more.than.21.rate.valid = sum(!is.na(more.than.21.rate)),
-            occupancy.rate = mean(occupancy.rate, na.rm=TRUE),
-            more.than.7.rate = mean(more.than.7.rate, na.rm=TRUE),
-            more.than.14.rate = mean(more.than.14.rate, na.rm=TRUE),
-            more.than.21.rate = mean(more.than.21.rate, na.rm=TRUE))
-
-# Weekly average on trust level
-Sitrep_weekly_average_bytrust <- Sitrep_daily %>%
-  group_by(nhs.england.region, code, name, STP, STP_code, week_start) %>%
-  # Count the number of valid observations for each variable
-  # BEFORE we overwrite the variables
-  summarize(occupancy.rate.valid = sum(!is.na(occupancy.rate)),
-            more.than.7.rate.valid = sum(!is.na(more.than.7.rate)),            
-            more.than.14.rate.valid = sum(!is.na(more.than.14.rate)),
-            more.than.21.rate.valid = sum(!is.na(more.than.21.rate)),
-            occupancy.rate = mean(occupancy.rate, na.rm=TRUE),
-            more.than.7.rate = mean(more.than.7.rate, na.rm=TRUE),
-            more.than.14.rate = mean(more.than.14.rate, na.rm=TRUE),
-            more.than.21.rate = mean(more.than.21.rate, na.rm=TRUE))
-
-
-# Weekly average on STP level
-Sitrep_weekly_average_bySTP <- Sitrep_weekly_average_bytrust %>%
-  filter(!is.element(code, trusts_to_exclude_for_aggregation)) %>%
-  group_by(STP, STP_code, week_start) %>%
-  summarise(occupancy.rate.valid = sum(!is.na(occupancy.rate)),
-            more.than.7.rate.valid = sum(!is.na(more.than.7.rate)),
-            more.than.14.rate.valid = sum(!is.na(more.than.14.rate)),
-            more.than.21.rate.valid = sum(!is.na(more.than.21.rate)),
-            occupancy.rate = mean(occupancy.rate, na.rm=TRUE),
-            more.than.7.rate = mean(more.than.7.rate, na.rm=TRUE),
-            more.than.14.rate = mean(more.than.14.rate, na.rm=TRUE),
-            more.than.21.rate = mean(more.than.21.rate, na.rm=TRUE))
-
+  mutate(month = format(date, format = "%B"))
 
 # Monthly average on trust level
 Sitrep_monthly_average_bytrust <- Sitrep_daily %>%
   group_by(nhs.england.region, code, name, STP, STP_code, month) %>%
-  # Count the number of valid observations for each variable
-  # BEFORE we overwrite the variables
-  summarize(occupancy.rate.valid = sum(!is.na(occupancy.rate)),
-            more.than.7.rate.valid = sum(!is.na(more.than.7.rate)),
-            more.than.14.rate.valid = sum(!is.na(more.than.14.rate)),
-            more.than.21.rate.valid = sum(!is.na(more.than.21.rate)),
-            occupancy.rate = mean(occupancy.rate, na.rm=TRUE),
-            more.than.7.rate = mean(more.than.7.rate, na.rm=TRUE),
-            more.than.14.rate = mean(more.than.14.rate, na.rm=TRUE),
-            more.than.21.rate = mean(more.than.21.rate, na.rm=TRUE))
+  summarize(occupancy.rate = mean(occupancy.rate, na.rm=TRUE))
 
 # Monthly average on STP level
 Sitrep_monthly_average_bySTP <- Sitrep_monthly_average_bytrust %>%
   filter(!is.element(code, trusts_to_exclude_for_aggregation)) %>%
   group_by(STP, STP_code, month) %>%
-  summarise(occupancy.rate.valid = sum(!is.na(occupancy.rate)),
-            more.than.7.rate.valid = sum(!is.na(more.than.7.rate)),
-            more.than.14.rate.valid = sum(!is.na(more.than.14.rate)),
-            more.than.21.rate.valid = sum(!is.na(more.than.21.rate)),
-            occupancy.rate = mean(occupancy.rate, na.rm=TRUE),
-            more.than.7.rate = mean(more.than.7.rate, na.rm=TRUE),
-            more.than.14.rate = mean(more.than.14.rate, na.rm=TRUE),
-            more.than.21.rate = mean(more.than.21.rate, na.rm=TRUE))
+  summarise(occupancy.rate = mean(occupancy.rate, na.rm=TRUE))
 
 
 # 7. Visualisation: creating maps in R ------------------------------------
 
-# Download and read file containing STP shapes from the ONS website
+# Load file containing STP shapes (originally from the ONS website)
 # We will use the smaller, generalised version for mapping rather than the full boundaries
 
-STP_geojson_filename <- "Sustainability_and_Transformation_Partnerships_February_2017_Ultra_Generalised_Clipped_Boundaries_in_England.geojson.json"
-STP_geojson_url <- "http://geoportal1-ons.opendata.arcgis.com/datasets/571bd4512165461fad70a0ccc36450e4_4.geojson"
+STP_geojson_filename <- "Sustainability_and_Transformation_Partnerships_February_2017_Ultra_Generalised_Clipped_Boundaries_in_England.geojson"
+# STP_geojson_url <- "http://geoportal1-ons.opendata.arcgis.com/datasets/571bd4512165461fad70a0ccc36450e4_4.geojson"
 
-download.file(STP_geojson_url, STP_geojson_filename)
 data_json <- geojson_read(STP_geojson_filename, what = "sp")
-
-plot(data_json)
-
 
 # to use ggplot for maps, we first need to turn sp data into a dataframe
 # make sure the region argument so as not to lose the STP identiers
@@ -347,7 +216,7 @@ STP_shape_monthly <- STP_shape_monthly %>%
                                      levels = rev(levels(occupancy.rate.cut)))) %>% 
   # Remove lines relating England as a whole
   filter(!is.na(STP)) %>% 
-  # Turn STPs and months into factors (ignoring March)
+  # Turn STPs and months into factors (ignoring November, March)
   mutate(id = factor(id, levels = unique(id)),
          month = factor(month, levels = c("December", "January", "February"))) %>% 
   filter(!is.na(month))
@@ -382,5 +251,4 @@ map_monthly_bedocc <- ggplot() +
         legend.key.size = unit(18, "mm"),
         strip.text = element_text(size = 16*ggplot2:::.pt, colour = "#524c48", margin = margin(b = 10))) 
 
-
-ggsave("Bedocc_monthly_map.png", map_monthly_bedocc, device = "png",  width = 650, height = 400, units = c("mm"))
+ggsave("Bedocc_monthly_map_1819.png", map_monthly_bedocc, device = "png",  width = 650, height = 400, units = c("mm"))
